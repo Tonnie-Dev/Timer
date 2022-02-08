83d01eee57c05e2575c24c8c8165498636f1870a
Antony <vontonnie@gmail.com>
2022-02-08 07:20:37 +0300

    Create Timer Composable

--- app/src/main/java/com/uxstate/timer/MainActivity.kt
+++ app/src/main/java/com/uxstate/timer/MainActivity.kt
@@ -3,13 +3,11 @@ package com.uxstate.timer
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import com.uxstate.timer.ui.theme.TimerTheme
 
 class MainActivity : ComponentActivity() {
@@ -18,9 +16,22 @@ class MainActivity : ComponentActivity() {
         setContent {
             TimerTheme {
 
-                }
             }
         }
     }
+}
 
+
+@Composable
+fun Timer(
+    totalTime: Long,
+    handleColor: Color,
+    activeBarColor: Color,
+    inactiveBarColor: Color,
+    modifier: Modifier = Modifier,
+    initialValue: Float = 0f,
+    strokeWidth: Dp = 5.dp
+) {
+
+}
 
