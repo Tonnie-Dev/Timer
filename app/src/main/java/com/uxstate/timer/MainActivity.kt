524caa362f35a487be3cbef97187080237cad056
Antony <vontonnie@gmail.com>
2022-02-08 19:10:58 +0300

    Draw Timer Text

--- app/src/main/java/com/uxstate/timer/MainActivity.kt
+++ app/src/main/java/com/uxstate/timer/MainActivity.kt
@@ -5,6 +5,7 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.Box
+import androidx.compose.material.Text
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -15,9 +16,11 @@ import androidx.compose.ui.graphics.PointMode
 import androidx.compose.ui.graphics.StrokeCap
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.layout.onSizeChanged
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.Dp
 import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.uxstate.timer.ui.theme.TimerTheme
 import java.lang.Math.PI
 import kotlin.math.cos
@@ -116,6 +119,14 @@ fun Timer(
             )
 
         })
+
+        //outside the canvas we draw a text to show time
+        Text(
+            text = (currentTime / 1000L).toString(),
+            color = Color.White,
+            fontSize = 44.sp,
+            fontWeight = FontWeight.Bold
+        )
     }
 
 
