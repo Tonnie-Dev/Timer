819c572a17cbe993edd62e56559661b6b88725ad
Antony <vontonnie@gmail.com>
2022-02-08 07:24:40 +0300

    Add States

--- app/src/main/java/com/uxstate/timer/MainActivity.kt
+++ app/src/main/java/com/uxstate/timer/MainActivity.kt
@@ -3,10 +3,11 @@ package com.uxstate.timer
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.Composable
+import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.IntSize
 import androidx.compose.ui.unit.dp
 import com.uxstate.timer.ui.theme.TimerTheme
 
@@ -33,5 +34,12 @@ fun Timer(
     strokeWidth: Dp = 5.dp
 ) {
 
+
+    //STATES
+    //Constructs an IntSize from width and height Int values
+    var size by remember{ mutableStateOf(IntSize.Zero)}
+    var value by remember{ mutableStateOf(initialValue)}
+    var currentTime by remember{ mutableStateOf(totalTime)}
+    var isTimerRunning by remember{ mutableStateOf(false)}
 }
 
